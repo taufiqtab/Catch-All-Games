3c21c114f409a5861161d4ff83b221b6313d6b2f
taufiqtab <taufiqtab77@gmail.com>
2017-12-14 23:36:17 +0700

    100 % Playable Without Sound

---
+++ Assets/_Scripts/ButtonListener.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonListener : MonoBehaviour {
+
+	public void OpenScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
